1ebd50eac87c66bcb046e1ee538211ceed82fef7
jose luis <joseluislopeztorres14@gmail.com>
2022-09-26 12:48:22 -0600

    index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- css only -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- stylesheet -->
+    <link rel="stylesheet" href="estilos.css">
+    <!-- javaScript  -->
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+    <title>Document</title>
+</head>
+<body class="t" style="background-color: skyblue";>
+<?php include("cabecera.php") ?>
+<center>
+<div>
+<h1 class="titulo" style="color: black";>EXPERENCIA DE USUARIO</h1>
+</div>
+<br><br>
+<div>
+    <img src="img/portada.png" alt="">
+</div>
+<br><br>
+
+<div class="texto">
+    <p >Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, reiciendis odio est incidunt <br>
+    quibusdam ullam sint aut nobis, culpa accusantium distinctio cum eum excepturi placeat natus <br>
+    sit. Pariatur, laudantium facilis?</p>
+</div>
+</center>
+</body>
+</html>
