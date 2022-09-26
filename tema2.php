60c3a004021f30d1ea9f018b135d51b502c28c1b
jose luis <joseluislopeztorres14@gmail.com>
2022-09-26 12:48:58 -0600

    tema2.php

---
+++ tema2.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <!-- css only -->
+      <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- stylesheet -->
+    <link rel="stylesheet" href="estilos.css">
+    <!-- javaScript  -->
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+    <title>Document</title>
+</head>
+<body style="background-color: skyblue";>
+<?php include("cabecera.php") ?>
+
+<center>
+<div>
+    <h1>TEMA 2</h1>
+</div>
+<div>
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quidem. <br>
+    Veritatis animi itaque reiciendis magnam provident accusantium <br>
+    consequatur fugit ratione dolor tempore aspernatur, culpa debitis a numquam voluptatem dolorem? Officiis?
+</p>
+
+
+    <button type="submit"><a href="index.php">Regresar</a></button>
+</div>
+</center>
+</body>
+</html>
