cf2b757abd9d40395311bce6e803e118c0ce7c80
jose luis <joseluislopeztorres14@gmail.com>
2022-09-27 17:25:39 -0600

    tema2.php

--- tema2.php
+++ tema2.php
@@ -16,16 +16,36 @@
 <?php include("cabecera.php") ?>
 
 <center>
+    <br><br>
 <div>
-    <h1>TEMA 2</h1>
+    <h1> El ciclo de desarrollo de un producto</h1>
 </div>
+<br>
+<br>
+<div>
+    <img src="img/c.png" alt="">
+</div>
+<br><br>
 <div>
 <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quidem. <br>
-    Veritatis animi itaque reiciendis magnam provident accusantium <br>
-    consequatur fugit ratione dolor tempore aspernatur, culpa debitis a numquam voluptatem dolorem? Officiis?
+Cada producto nuevo, sea una aplicación o un objeto físico, sigue una serie de pasos <br>
+específicos que lo llevan desde la primera chispa de una idea hasta el lanzamiento del producto final. <br>
+Esto se denomina ciclo de desarrollo del producto y tiene cinco etapas: lluvia de ideas, definición, <br>
+ diseño, prueba y lanzamiento. En función de dónde trabajes, los nombres exactos de cada etapa pueden <br>
+  ser un poco diferentes, pero el proceso general suele ser el mismo.
 </p>
-
+<br>
+<p><b>
+Lluvia de ideas  </b><br>
+La primera etapa del ciclo de desarrollo de un producto es la de lluvia de ideas, cuando el <br>
+equipo empieza a pensar en una idea para un producto. En algunos casos, tú y tu equipo ya conocerán <br>
+el problema del usuario que quieren resolver al empezar el ciclo de desarrollo del producto. <br>
+Si no es así, crear una lista de problemas del usuario es un excelente punto de partida. <br>
+Es importante prestar atención a la diversidad del equipo en esta etapa. Los equipos que  <br>
+tienen una diversidad rica desde distintos puntos de vista, como la raza, el género, las destrezas, <br>
+ la estructura familiar, la edad y la etnia, suelen ser más eficaces en la lluvia de ideas, porque reúnen muchas experiencias de vida diferentes. 
+</p>
+<br><br><br>
 
     <button type="submit"><a href="index.php">Regresar</a></button>
 </div>
